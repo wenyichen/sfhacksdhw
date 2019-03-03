79e7b8af1a2b7a107d0e8d94276e460ca07c05cb
Wenyi Chen <wenyichen@outlook.com>
2019-03-02 17:36:14 -0800

    implement endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from google.cloud import storage
 from flask import Flask, render_template, request, abort, jsonify
+import requests
 
 PROJECT_NAME='sfhacksdhw'
 BUCKET_NAME='sfhacksdhw.appspot.com'
@@ -9,7 +10,6 @@ app = Flask(__name__)
 #login_manager.init_app(app)
 
 @app.route('/')
-@app.route('/index')
 def index():
     user = 'Hello there'
     return render_template('index.html', user = user)
@@ -28,15 +28,6 @@ def profiles():
         }
     ]
     return render_template('profiles.html', user = user, profiles = profiles)
-
-#@app.route('/login', methods=['GET','POST'])
-#def login():
-#    form = LoginForm()
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/user/<username>', methods=['POST'])
 def createUser(username):
@@ -62,31 +53,42 @@ def getUser(username):
     if not blob.exists():
         #fail
         abort(400)
-    blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+    blobs = list(bucket.list_blobs(prefix=user_string))
+    links = [jsonify(url=b.name, imgurl=b.generate_signed_url(86400)) for b in blobs]
     #success: user created without problems
-    return jsonify(success=True)
+    return jsonify(success=True,urls=links)
 
 @app.route('/user/<username>/url/<url>', methods=['POST'])
 def addUrl(username, url):
     # Make sure we have an url in the request
     if not url or not username:
-        
-        pass
-    
+        abort(400)
+    gcs_client = storage.Client(project=PROJECT_NAME)
+    bucket = gcs_client.get_bucket(BUCKET_NAME)
+    user_string = username + "/"
+    blob = bucket.blob(user_string)
+    if not blob.exists():
+        #fail
+        abort(400)
+    # if url already exists
+    url_string = username + "/" + url
+    blob = bucket.blob(url_string)
+    if blob.exists():
+        abort(400)
+
     # turn url to qrcode img
+    img = requests.get("https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + url).content
 
     # add url/qrcode img to user
-    pass
+    blob.upload_from_file(img, content_type='PNG')
+    return jsonify(success=True)
 
 @app.route('/user/<username>/qr', methods=['POST'])
 def addQr(username): 
     # Make sure we have an file in the request
-    img = request.file.get('url', None)
-
+    img = request.file.get('img', None)
     if not img or not username:
-        #fail
-        pass
-    
+        abort(400)
     # turn img to url
 
     # turn url to qrcode img
